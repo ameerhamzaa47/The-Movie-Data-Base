a75cfbedba7f465f272d0a24a7edb032a05bd1bf
Ameer Hamza <hamzaataariq12@gmail.com>
2024-12-06 11:12:53 +0500

    error removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
@@ -13,6 +15,6 @@ export default {
   },
   darkMode: "selector",
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
