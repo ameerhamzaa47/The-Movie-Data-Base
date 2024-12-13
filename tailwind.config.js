34a393a06076d5a28e693aa8a15e97631c99f0c7
Ameer Hamza <hamzaataariq12@gmail.com>
2024-12-14 00:23:40 +0500

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const daisyui = require('daisyui');
-const tailwindScrollbarHide = require('tailwind-scrollbar-hide');
+import daisyui from 'daisyui';
 
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -20,34 +19,6 @@ module.exports = {
   darkMode: "selector",
   plugins: [
     daisyui,
-    tailwindScrollbarHide,
+    // require('tailwind-scrollbar-hide'),
   ],
 }
-
-
-
-
-// /** @type {import('tailwindcss').Config} */
-// import daisyui from 'daisyui';
-
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       fontFamily: {
-//         Poppins: ['Roboto', 'sans-serif'],
-//       },
-//       boxShadow: {
-//         'left': '-10px 0px 15px rgba(0, 0, 0, 0.3)',
-//       },
-//     },
-//   },
-//   darkMode: "selector",
-//   plugins: [
-//     daisyui,
-//     require('tailwind-scrollbar-hide'),
-//   ],
-// }
